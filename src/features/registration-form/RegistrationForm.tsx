e4b2f8e50a5929641236cc060b51e60564b6f21e
ilovesg <immortalbeing13@gmail.com>
2022-10-18 22:52:22 +0500

    Implement step changing

--- src/features/registration-form/RegistrationForm.tsx
+++ src/features/registration-form/RegistrationForm.tsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
-import { Form } from "react-final-form";
+import { Form, Field } from "react-final-form";
 import RegistrationFormItems from "./RegistrationFormItems";
-import { logPassFields } from "./registrationFormData";
+import {
+  logPassFields,
+  addressFields,
+  phoneNumberFields,
+} from "./registrationFormData";
 import { IRegistrationFormField } from "./types/RegistrationFormFieldInterface";
 
 export default function RegistrationForm() {
   const [step, setStep] = useState<number>(1);
 
-  let fields: IRegistrationFormField[] = [];
+  const fieldsArr: IRegistrationFormField[][] = [
+    logPassFields,
+    addressFields,
+    phoneNumberFields,
+  ];
 
-  switch (step) {
-    default:
-      fields = logPassFields;
-  }
+  const changeStep = (delta: number) => () => {
+    const nextStep = step + delta;
+
+    if (nextStep < 1) {
+      setStep(1);
+
+      return;
+    }
+
+    if (nextStep > fieldsArr.length) {
+      setStep(fieldsArr.length);
+
+      return;
+    }
+
+    setStep(nextStep);
+  };
 
   const onSubmit = (): void => {
     alert("Submitted!");
@@ -21,13 +42,23 @@ export default function RegistrationForm() {
   return (
     <Form
       onSubmit={onSubmit}
-      render={({ handleSubmit }) => (
+      render={({ handleSubmit, values }) => (
         <form onSubmit={handleSubmit}>
-          <RegistrationFormItems fields={fields} />
-          <button type="button" className="btn btn-primary me-3">
+          <RegistrationFormItems fields={fieldsArr[step - 1]} />
+          <button
+            type="button"
+            className="btn btn-primary me-3"
+            onClick={changeStep(-1)}
+            disabled={step === 1}
+          >
             Prev
           </button>
-          <button type="button" className="btn btn-primary me-3">
+          <button
+            type="button"
+            className="btn btn-primary me-3"
+            onClick={changeStep(1)}
+            disabled={step === fieldsArr.length}
+          >
             Next
           </button>
           <button type="submit" className="btn btn-primary">
