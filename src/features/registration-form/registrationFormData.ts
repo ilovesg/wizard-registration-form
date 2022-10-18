0133fa9cb9f024fcdfb30a55ed3d8042f1d35400
ilovesg <immortalbeing13@gmail.com>
2022-10-18 21:02:10 +0500

    Add registration form field data

---
+++ src/features/registration-form/registrationFormData.ts
@@ -0,0 +1,73 @@
+import { IRegistrationFormField } from "./types/RegistrationFormFieldInterface";
+
+export const logPassFields: IRegistrationFormField[] = [
+  {
+    field: "login",
+    required: true,
+    label: "Login",
+    type: "text",
+    placeholder: "Login",
+  },
+  {
+    field: "email",
+    required: true,
+    label: "Email",
+    type: "email",
+    placeholder: "Email",
+  },
+  {
+    field: "password",
+    required: false,
+    label: "Password",
+    type: "password",
+    placeholder: "Password",
+  },
+  {
+    field: "repeatPassword",
+    required: false,
+    label: "Repeat password",
+    type: "password",
+    placeholder: "Repeat password",
+  },
+];
+
+export const addressFields: IRegistrationFormField[] = [
+  {
+    field: "country",
+    required: false,
+    label: "Country",
+    type: "text",
+    placeholder: "Country",
+  },
+  {
+    field: "city",
+    required: false,
+    label: "City",
+    type: "text",
+    placeholder: "City",
+  },
+  {
+    field: "street",
+    required: false,
+    label: "Street",
+    type: "text",
+    placeholder: "Street",
+  },
+  {
+    field: "building",
+    required: false,
+    label: "Building",
+    type: "number",
+    placeholder: "Building",
+  },
+];
+
+export const phoneNumberFields: IRegistrationFormField[] = [
+  {
+    field: "phone",
+    required: false,
+    label: "Phone number",
+    type: "tel",
+    placeholder: "Phone number",
+  },
+];
