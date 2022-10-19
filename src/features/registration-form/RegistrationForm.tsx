6636a110082150c527869b2baa9577fd68f3ebd5
ilovesg <immortalbeing13@gmail.com>
2022-10-20 02:54:54 +0500

    Implement saving form step and values

--- src/features/registration-form/RegistrationForm.tsx
+++ src/features/registration-form/RegistrationForm.tsx
@@ -10,7 +10,7 @@ import { IRegistrationFormField } from "./types/RegistrationFormFieldInterface";
 import RegistrationFormPhoneVerification from "./RegistrationFormPhoneVerification";
 
 export default function RegistrationForm() {
-  const [step, setStep] = useState<number>(1);
+  const [step, setStep] = useState<number>(+localStorage.step || 1);
   const [code, setCode] = useState<string>("");
 
   const fieldsArr: IRegistrationFormField[][] = [
@@ -25,14 +25,20 @@ export default function RegistrationForm() {
     if (nextStep < 1) {
       setStep(1);
 
+      localStorage.setItem("step", "1");
+
       return;
     }
 
     if (nextStep > fieldsArr.length) {
       setStep(fieldsArr.length);
 
+      localStorage.setItem("step", String(fieldsArr.length));
+
       return;
     }
+
+    localStorage.setItem("step", String(nextStep));
 
     setStep(nextStep);
   };
@@ -49,29 +55,39 @@ export default function RegistrationForm() {
 
   return (
     <Form
+      initialValues={
+        (localStorage.values && JSON.parse(localStorage.values)) || {}
+      }
       onSubmit={onSubmit}
       render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit} noValidate>
-          <RegistrationFormItems fields={fieldsArr[step - 1]}>
-            {step === fieldsArr.length && (
-              <RegistrationFormPhoneVerification
-                code={code}
-                setCode={setCode}
-              />
-            )}
-          </RegistrationFormItems>
-          <button
-            type="button"
-            className="btn btn-primary me-3"
-            disabled={step === 1}
-            onClick={() => changeStep(-1)}
-          >
-            Prev
-          </button>
-          <button type="submit" className="btn btn-primary me-3">
-            {isLastStep ? "Submit" : "Next"}
-          </button>
-        </form>
+        <>
+          <FormSpy
+            onChange={(formState) => {
+              localStorage.setItem("values", JSON.stringify(formState.values));
+            }}
+          />
+          <form onSubmit={handleSubmit} noValidate>
+            <RegistrationFormItems fields={fieldsArr[step - 1]}>
+              {step === fieldsArr.length && (
+                <RegistrationFormPhoneVerification
+                  code={code}
+                  setCode={setCode}
+                />
+              )}
+            </RegistrationFormItems>
+            <button
+              type="button"
+              className="btn btn-primary me-3"
+              disabled={step === 1}
+              onClick={() => changeStep(-1)}
+            >
+              Prev
+            </button>
+            <button type="submit" className="btn btn-primary me-3">
+              {isLastStep ? "Submit" : "Next"}
+            </button>
+          </form>
+        </>
       )}
     />
   );
